855dd6932b88651cce8657585f5296810b4275ea
phillongworth <PHIL.LONGWORTH@GMAIL.COM>
2026-01-10 21:13:57 +0000

    revised deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,5 @@
+$SERVER = "myserver"
 $REMOTE_PATH = "/var/www/phillongworth-site/html"
+
+# This line uses the variables above
 scp -r ./* "${SERVER}:${REMOTE_PATH}"
-# ... rest of script targeting ONLY the .site folder ...
